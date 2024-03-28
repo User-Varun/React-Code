113e84f70fad1492bda7e206ea9fd49376805249
Varun <varunpawardigital@gmail.com>
2024-03-29 02:35:05 +0530

    learned about state and made a little project!

---
+++ 05-steps/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "vaurn" });
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+
+    // Bad practice
+    // test.name = "fred";
+    // Best Practice
+    setTest({ name: "fred" });
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        {/* thats pretty clever! 😉 [step - 1] logic. */}
+        Step {step}: {messages[step - 1]} {test.name}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
