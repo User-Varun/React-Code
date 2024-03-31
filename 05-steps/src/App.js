6c6025c27e0d6508e583ac76811ffc64e88a9154
Varun <varunpawardigital@gmail.com>
2024-03-31 21:20:05 +0530

    completed the lecture portion , next is challenge no.1

--- 05-steps/src/App.js
+++ 05-steps/src/App.js
@@ -7,47 +7,74 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
-  const [test, setTest] = useState({ name: "vaurn" });
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test, setTest] = useState({ name: "vaurn" });
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      // setStep( s + 1); this does not work when we call it 2 times. (why...? is later.)
+      // setStep((s) => s + 1);
+      setStep((s) => s + 1);
+      // to be safe always update values using a callback functions
+    }
 
     // Bad practice
     // test.name = "fred";
     // Best Practice
-    setTest({ name: "fred" });
+    // setTest({ name: "fred" });
+  }
+
+  function handleClose() {
+    setIsOpen((isOpen) => !isOpen);
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        {/* thats pretty clever! 😉 [step - 1] logic. */}
-        Step {step}: {messages[step - 1]} {test.name}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
+    <div>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            {/* thats pretty clever! 😉 [step - 1] logic. */}
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
 
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
