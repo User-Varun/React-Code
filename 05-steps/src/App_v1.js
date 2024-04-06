c9ca4443ab9e6c3283e1b347cf88909259edd586
Varun <varunpawardigital@gmail.com>
2024-04-06 20:53:01 +0530

    learned about the children prop!

--- 05-steps/src/App_v1.js
+++ 05-steps/src/App_v1.js
@@ -7,7 +7,12 @@ const messages = [
 ];
 
 export default function App() {
-  return <Steps />;
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
 }
 
 function Steps() {
@@ -54,28 +59,22 @@ function Steps() {
             {/* {test.name} */}
           </p>
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
-              {" "}
-              <span>👈</span>Previous{" "}
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
-              {" "}
-              Next <span>👉</span>{" "}
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
